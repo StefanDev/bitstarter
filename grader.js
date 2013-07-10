d554aec9853df12a4b9212c6aa3f15787c664678
Ubuntu <ubuntu@ip-172-31-34-158.us-west-2.compute.internal>
2013-07-10 00:03:59 +0000

    implemented url

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var NOURL = "NONE";
+var restler = require("restler");
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -40,19 +42,27 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioContent = function(urlcontent) {
+    return cheerio.load(urlcontent);
+}
+
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+//    $ = cheerioHtmlFile(htmlfile);
+     $ = cheerioContent(htmlfile);
+    console.log("cheerio function:");
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
-    return out;
+//    return out;
+    checkJsonFile(out);
 };
 
 var clone = function(fn) {
@@ -61,14 +71,39 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkJsonFile = function(checkJson){
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <url>', 'url to index.html', NOURL)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+        console.log("is url"+program.url);
+
+    if(program.url != NOURL){
+        restler.get(program.url).on('complete', function(result) {
+             console.log("doing url...");
+           if (result instanceof Error) {
+             sys.puts('Error: ' + result.message);
+           } else {
+             var remotefile = result;
+             checkHtmlFile(remotefile, program.checks);
+           }
+         });
+    } else {
+         program.file = HTMLFILE_DEFAULT;
+         var content = fs.readFileSync(program.file);
+         checkHtmlFile(content, program.checks);
+   }
+
+  //  var outJson = JSON.stringify(checkJson, null, 4);
+    //console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
